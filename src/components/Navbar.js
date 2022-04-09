4b1ecde700a2cbafc5057db4a156b788ef1cea15
Arundhati <arundhatiraicar12@gmail.com>
2022-04-10 02:38:28 +0530

    Rough navlinks added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,41 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
+import { NavLink} from "react-router-dom"
 
+const linkStyles = {
+    display: "inline-block",
+    width: "170px",
+    padding: "15px",
+    margin: "0px 22px 0px 2px",
+    background:"pink",
+    textDecoration: "none",
+    color: "white",
+    fontSize:"20px",
+    fontFamily:"serif",
+    
+    
+    
+    
+  }
 function Navbar(){
 return (
     <>
      <h1>Shades of Fashion</h1>
      <navbar>
-    <h2>Navbar</h2>
-          Find User
+     <NavLink to="/" exact style={linkStyles} activeStyle={{background: "#4E0099",}}>
+        Home
+    </NavLink>
      
-        
-          Add User
-        
-      
-          Home
+    
+    <NavLink to="/users" exact style={linkStyles} activeStyle={{background: "#4E0099",}}>
+       Users
+    </NavLink>
+    <NavLink to="/addUser" exact style={linkStyles} activeStyle={{background: "#4E0099",}}>
+       Create a Profile
+    </NavLink>
+     
+    
+     
        
      </navbar>
     </>
