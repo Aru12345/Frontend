5fee9948c3e31bb8ea70e89d8c250d208434007b
Arundhati <arundhatiraicar12@gmail.com>
2022-04-11 23:36:29 +0530

    Home done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,13 @@ import styled from "styled-components";
 function Home(){
     return( 
         <HomeContainer>
-        
         <h3>View the latest fashion trends,show off your styles and much more....</h3>
-       <img src="https://assets.vogue.com/photos/5c86c8c29914d62d7ceb129a/master/w_2560%2Cc_limit/00-VW-top-f19-models.jpg" />
-       
-       
+        <img src="https://assets.vogue.com/photos/5c86c8c29914d62d7ceb129a/master/w_2560%2Cc_limit/00-VW-top-f19-models.jpg" />
        </HomeContainer>
     )
 }
 export default Home; 
+
 const HomeContainer =styled.div`
 display: flex;
 justify-content: center;
@@ -29,6 +27,4 @@ justify-content: center;
     font-size:300%;
     padding:5%
 }
-
-
 `
