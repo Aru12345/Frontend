9df0f76311375e64db74b69ca776746163abd379
Arundhati <arundhatiraicar12@gmail.com>
2022-04-24 12:14:10 +0530

    Seach roughly styled

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,9 @@
+
+import styled from "styled-components";
 function Search({searchTerm,onSearchChange}){
     return(
-        <>
-        <h1>search</h1>
+        <SearchContainer>
+      
         <label htmlFor="search"><h2>Search:</h2></label>
         <input
           type="text"
@@ -10,7 +12,27 @@ function Search({searchTerm,onSearchChange}){
           value={searchTerm}
           onChange={(e) => onSearchChange(e.target.value)}
         />
-        </>
+        </SearchContainer>
     )
 }
 export default Search;
+
+const SearchContainer=styled.div`
+
+& h2{
+  font-family:monospace;
+  font-size:230%;
+  color:white;
+  margin:1%
+}
+& input{
+  border-radius: 5px;
+    height: 20px;
+    width: 80%;
+    padding: 2px 20px 2px 30px;
+    outline: 0;
+    background-color: #f5f5f5;
+    margin:0.1%
+}
+
+`
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,14 +1,18 @@
 import userEvent from "@testing-library/user-event";
-
+import styled from "styled-components";
 function UserCard({user}){
     const{id,image,name,about}=user;
     return(
-        <>
+        <UserContainer>
         <img src={image} />
         <h2>Name:{name}</h2>
         <p>About:{about}</p>
 
-        </>
+        </UserContainer>
     )
 }
 export default UserCard;
+
+const UserContainer=styled.div`
+
+`
