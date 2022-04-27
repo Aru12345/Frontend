0ff07a99ab73c5fbf7bd0fe642b0e69b70b2dd88
Arundhati <arundhatiraicar12@gmail.com>
2022-04-27 19:48:35 +0530

    ReadMe updated

--- src/components/AddReviewForm.js
+++ src/components/AddReviewForm.js
@@ -1,6 +1,41 @@
 import React, { useState } from 'react'
 
-function AddReviewForm(){
+function AddReviewForm({user,onAddReview,hideForm}){
+    const [formData, setFormData] = useState({
+    
+        image:'',
+        product:'',
+        link:'',
+        price:'',
+        size:'',
+        styleNote:''
+      })
+
+  function handleChange(e){
+    const newFormData = {
+      ...formData, [e.target.name]: e.target.value
+    }
+    setFormData(newFormData)
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+    const itemID = member.reviewList.length + 1
+    const newItem = {...formData, id:itemID}
+    const {id, wishlist} = member
+    const newWishList = [...wishlist, newItem]
+    const updatedMember = {...member, wishlist:newWishList}
+    onAddItem  && onAddItem(updatedMember)
+    setFormData({
+      id: 0,
+      item: "",
+      price: "$",
+      url: "",
+      desc: "",
+      specs: "",
+      store: "",
+      purchased: false
+    })
+  }
     return(
         <>
         <h1>Add item form</h1>
--- src/components/DisplayUser.js
+++
@@ -1,8 +0,0 @@
-function DisplayUser(){
-    return (
-        <>
-        <h1>DisplayUser</h1>
-        </>
-    )
-}
-export  default DisplayUser;
